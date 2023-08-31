9e046fba4622ffa6b1438279f04616359c255a5e
Lao <flavio.lao@gmail.com>
2023-08-31 08:28:29 -0300

    Add files via upload

---
+++ HotelAlura/src/main/java/lao/hotel/db/ConnectionFactory.java
@@ -0,0 +1,27 @@
+package lao.hotel.db;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    public DataSource dataSource;
+    public ConnectionFactory(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotel?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+
+        //Número de conexoes
+        comboPooledDataSource.setMaxPoolSize(15);
+
+        this.dataSource = comboPooledDataSource;
+    }
+
+    public Connection Conexao() throws SQLException {
+        return this.dataSource.getConnection();
+    }
+}
